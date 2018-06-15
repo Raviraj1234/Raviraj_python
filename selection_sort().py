3b5182bc257fdc0775c575be0b7dcf7f0c4f9da2
Raviraj1234 <40282916+Raviraj1234@users.noreply.github.com>
2018-06-14 18:57:56 -0700

    Selection_Sort Using Recursion in python
    Selection_Sort Using Recursion in python

---
+++ selection_sort().py
@@ -0,0 +1,27 @@
+"""def selection_Sort():
+    n=raw_input("\nEnter the numbersize")
+    l=[]
+    for i in range(int(n)):
+        x=raw_input("Enter %d element" %i)
+        l.append(x)
+
+    for i in range(len(l)):
+        mini=l[i]
+        for j in range(i,len(l)):
+            
+
+            if(l[j]<mini):
+                mini=l[j]
+        z=l.index(mini)
+        l[i],l[z]=mini,l[i]
+
+
+    for j in l:
+        print(j),
+                       
+                       
+   """
+l=[5,6,0,3,1,2]
+print (max(l[2:len(l)]))
+                
+
